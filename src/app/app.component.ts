ad63bb976803c6bcc4c2bfb10841e505c20d58de
ringokiak <ringokiak@gmail.com>
2022-03-20 10:33:11 +0800

    初始化

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
+/*
+ * @Author: r1ngo
+ * @Date: 2022-03-20 10:12:21
+ * @LastEditors: r1ngo
+ * @LastEditTime: 2022-03-20 10:15:07
+ * @Description: ez pz lemon squezzy
+ */
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root', // 组件名
+  templateUrl: './app.component.html', // 组件关联的html
+  styleUrls: ['./app.component.scss'], // 组件关联的css文件
 })
 export class AppComponent {
   title = 'angular-practice';
